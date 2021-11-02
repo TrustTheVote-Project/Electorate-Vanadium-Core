9f1c0af449b204fae604794497bf790525115d1b
Ion Y <ion@osetinstitute.org>
2021-11-02 11:53:06 -0700

    Model tests: Voters.

--- test/vanadium/tests/test_models.py
+++ test/vanadium/tests/test_models.py
@@ -41,6 +41,117 @@ FILE_TESTS = [
 
 
 # dict, exception context
+VOTER_TESTS = [
+    (
+        {},
+        RAISES_MISSING
+    ),
+    (
+        {
+            "name":
+            {
+                "full_name": "First Last"
+            },
+        },
+        RAISES_NONE
+    ),
+    (
+        {
+            "name":
+            {
+                "first_name": "Jane",
+                "last_name": "Doe",
+                "middle_name": [ "A" ],
+            },
+        },
+        RAISES_NONE
+    ),
+    (
+        {
+            "name": Name(
+                first_name = "Jane",
+                last_name = "Doe",
+                middle_name = [ "A" ],
+            ),
+        },
+        RAISES_NONE
+    ),
+    (
+        {
+            "contact_method": [
+                {
+                    "type": ContactMethodType.EMAIL,
+                    "value": "jane.doe@email.com",
+                },
+                {
+                    "type": ContactMethodType.PHONE,
+                    "value": "999-555-1111",
+                },
+                {
+                    "type": ContactMethodType.OTHER,
+                    "value": "Bat signal",
+                },
+            ],
+            "date_of_birth": "2001-07-05",
+            "ethnicity": "White",
+            "gender": "Female",
+            # "mailing_address": None,
+            "name": {
+                "first_name": "Jane",
+                "last_name": "Doe",
+                "middle_name": [ "A" ],
+            },
+            "party": {
+                "name": "Independent",
+            },
+            # "previous_residence_address": None,
+            "previous_signature": {
+                "source": SignatureSource.VOTER,
+                "type": SignatureType.OTHER,
+                "other_type": "photograph",
+            },
+            # "residence_address": None,
+            "residence_address_is_mailing_address": True,
+            "signature": {
+                "date": "2020-02-29",
+                "file_value": {
+                    "data": b"W2ltYWdlIGZpbGVd",
+                    "file_name": "signature.png",
+                    "mime_type": "image/png",
+                },
+                "source": SignatureSource.DMV,
+                "type": SignatureType.ELECTRONIC,
+            },
+            "voter_classification": [
+                {
+                    "assertion": AssertionValue.YES,
+                    "type": VoterClassificationType.UNITED_STATES_CITIZEN,
+                },
+                {
+                    "assertion": AssertionValue.UNKNOWN,
+                    "type": VoterClassificationType.RESTORED_FELON,
+                },
+            ],
+            "voter_id": [
+                {
+                    "attest_no_such_id": False,
+                    "date_of_issuance": "2017-09-22",
+                    "file_value": {
+                        "data": b"W2ltYWdlIGZpbGVd",
+                        "file_name": "drivers-license.txt",
+                        "mime_type": "text/plain",
+                    },
+                    "string_value": "X1234567",
+                    "type": VoterIdType.DRIVERS_LICENSE,
+                }
+            ]
+        },
+        RAISES_NONE
+    )
+]
+
+
+# dict, exception context
 VOTER_RECORDS_REQUEST_TESTS = [
     (
         {},
@@ -114,6 +225,7 @@ VOTER_RECORDS_REQUEST_TESTS = [
     ),
 ]
 
+
 # --- Test cases
 
 @pytest.mark.parametrize("data,raises", FILE_TESTS)
@@ -124,6 +236,13 @@ def test_file(data, raises):
         assert model.data == data["data"]
         assert model.file_name == data.get("file_name")
         assert model.mime_type == data.get("mime_type")
+
+
+@pytest.mark.parametrize("data,raises", VOTER_TESTS)
+def test_voter(data, raises):
+    raises, args, opts = raises
+    with raises(*args, **opts) as ex:
+        model = Voter(**data)
 
 
 @pytest.mark.parametrize("data,raises", VOTER_RECORDS_REQUEST_TESTS)
