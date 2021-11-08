0e87e74a9c903f8d9e504a5ac0298882717344f3
Ion Y <ion@osetinstitute.org>
2021-11-08 10:04:22 -0800

    Use 'importlib.resources' to load test data.

--- test/vanadium/tests/conftest.py
+++ test/vanadium/tests/conftest.py
@@ -1,4 +1,7 @@
 from contextlib import nullcontext as raises_none
+import json
+
+import importlib_resources
 
 from pytest import raises
 from pydantic import ValidationError
@@ -19,3 +22,13 @@ class Raises:
     MISSING = (raises, (ValidationError,), {"match": "value_error.missing"})
     # Unexpected fields in value, and additional properties forbidden.
     EXTRA = (raises, (ValidationError,), {"match": "value_error.extra"})
+
+
+def load_test_data(path, name):
+    """Load test data"""
+    path = path.replace("/", ".")
+    package = f"tests.data.{path}"
+    file = f"{name}.json"
+    path = importlib_resources.files(package).joinpath(file)
+    data = json.loads(path.read_text())
+    return data
