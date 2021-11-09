a00149de1ce543a19ecb97e57151f11e88e1312b
Ion Y <ion@osetinstitute.org>
2021-11-08 22:48:22 -0800

    Use 'VoterRecordResponse' and its subtypes as the response model.

--- test/vanadium/tests/routes/test_voter_registration.py
+++ test/vanadium/tests/routes/test_voter_registration.py
@@ -1,12 +1,19 @@
 from fastapi import FastAPI
+# NOTE: These tests are not proper unit tests.
+# They are *order dependent* and rely on the fact tat PyTest executes tests
+# in the order they are defined. This will be fixed but for now they are at
+# least testing the core API.
+
 from fastapi.testclient import TestClient
 
 import pytest
 
 from vanadium.app.main import application
 from vanadium.model import (
+    RequestError,
     RequestForm,
     RequestMethod,
+    SuccessAction,
     Voter,
     VoterRequestType,
     VoterRecordsRequest,
@@ -34,10 +41,12 @@ VOTER_RECORDS_REQUEST_TESTS = [
 def test_voter_registration_request(package, file):
     url = "/voter/registration/"
     body = load_test_data(package, file)
+    transaction_id = body["TransactionId"]
     response = client.post(url, json = body)
     assert response.status_code == 200
     data = response.json()
-    assert data["id"] == body["TransactionId"]
+    assert data["response"]["TransactionId"] == transaction_id
+    assert data["response"]["Action"][0] == SuccessAction.REGISTRATION_CREATED.value
     assert data["status"] == "Success"
     assert data["summary"].find("created") != -1
 
@@ -50,6 +59,7 @@ def test_voter_registration_request_without_transaction_id(package, file):
     response = client.post(url, json = body)
     assert response.status_code == 200
     data = response.json()
+    assert data["response"]["Action"][0] == SuccessAction.REGISTRATION_CREATED.value
     assert data["status"] == "Success"
     assert data["summary"].find("created") != -1
 
@@ -62,7 +72,7 @@ def test_voter_registration_check_status(package, file):
     response = client.get(url)
     assert response.status_code == 200
     data = response.json()
-    assert data["id"] == transaction_id
+    assert data["response"]["TransactionId"] == transaction_id
     assert data["status"] == "Success"
     assert data["summary"].find("in process") != -1
 
@@ -75,7 +85,8 @@ def test_voter_registration_update(package, file):
     response = client.put(url, json = body)
     assert response.status_code == 200
     data = response.json()
-    assert data["id"] == transaction_id
+    assert data["response"]["TransactionId"] == transaction_id
+    assert data["response"]["Action"][0] == SuccessAction.REGISTRATION_UPDATED.value
     assert data["status"] == "Success"
     assert data["summary"].find("updated") != -1
 
@@ -88,6 +99,7 @@ def test_voter_registration_cancel(package, file):
     response = client.delete(url)
     assert response.status_code == 200
     data = response.json()
-    assert data["id"] == transaction_id
+    assert data["response"]["TransactionId"] == transaction_id
+    assert data["response"]["Action"][0] == SuccessAction.REGISTRATION_CANCELLED.value
     assert data["status"] == "Success"
     assert data["summary"].find("cancelled") != -1
