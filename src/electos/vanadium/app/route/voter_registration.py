d64a5c7ac2dfce5fd867be211428a4edee67b721
Ion Y <ion@osetinstitute.org>
2021-11-08 22:48:22 -0800

    Use 'VoterRecordResponse' and its subtypes as the response model.

--- src/electos/vanadium/app/route/voter_registration.py
+++ src/electos/vanadium/app/route/voter_registration.py
@@ -1,10 +1,18 @@
-from typing import Optional
+from typing import Optional, Union
 
 from fastapi import APIRouter
 from pydantic import BaseModel
 
 from vanadium.app.resource import Resources
-from vanadium.model import VoterRecordsRequest
+from vanadium.model import (
+    Error,
+    RequestAcknowledgement,     # VoterRecordsResponse sub-class
+    RequestError,
+    RequestRejection,           # VoterRecordsResponse sub-class
+    RequestSuccess,             # VoterRecordsResponse sub-class
+    SuccessAction,
+    VoterRecordsRequest,
+)
 from vanadium.utils import UniqueIds
 
 
@@ -12,14 +20,9 @@ from vanadium.utils import UniqueIds
 
 _router = APIRouter(prefix = "/voter/registration")
 
-# NOTE: These tests are not proper unit tests.
-# They are *order dependent* and rely on the fact tat PyTest executes tests
-# in the order they are defined.
-
-
 @_router.post(
     "/",
-    # response_model = # VoterRecordsResponse
+    # response_model = VoterRecordsResponse,
     response_description = "Voter registration response",
     summary = "Initiate a new voter registration request",
 )
@@ -34,22 +37,39 @@ def voter_registration_request(
     # Otherwise generate one from the current time.
     # (Note: not robust just good enough for testing.)
     storage = Resources.get_storage()
-    unique_id = storage.insert(item.transaction_id, item)
-    if unique_id:
+    registration_id = storage.insert(item.transaction_id, item)
+    if registration_id:
         status = "Success"
         summary = "Voter registration request created"
+        response = RequestSuccess(
+            Action = [
+                SuccessAction.REGISTRATION_CREATED,
+            ],
+            TransactionId = registration_id
+        )
     else:
         status = "Failure"
         summary = "Voter registration request already exists"
+        response = RequestRejection(
+            AdditionalDetails = [
+                "The transaction ID is already associated to a pending request."
+            ],
+            Error = Error(
+                Name = RequestError.IDENTITY_LOOKUP_FAILED
+            ),
+
+            TransactionId = registration_id
+        )
     return {
-        "id":      unique_id,
-        "status":  status,
-        "summary": summary,
+        "status":   status,
+        "summary":  summary,
+        "response": response,
     }
 
 
 @_router.get(
     "/{transaction_id}",
+    # response_model = Union[RequestAcknowledgement, RequestRejection],
     summary = "Check on the status of a pending voter registration request"
 )
 def voter_registration_status(
@@ -62,10 +82,20 @@ def voter_registration_status(
     Parameters:
         `transaction_id`: The transaction ID as returned in the initial transaction.
 
+    Returns:
+        `RequestAcknowledgement` on success
+            Note that this type doesn't allow returning any details about the status.
+        `RequestRejection` on failure.
+
+        Note: the `response` field has the official response.
+        The `status` and `summary` provide additional information.
+
     Limitations:
 
-    - Only updates existing *request*. It does not update a registration
+    - Only updates an existing *request*. It does not update a registration
       that has been accepted or rejected, only one that is pending.
+      This isn't the correct behavior but it describes the current semantics.
+      (The reason is that there isn't yet any long-term storage.)
     - Lookup is only done through the transaction ID, not through other identifiers.
     """
     storage = Resources.get_storage()
@@ -73,13 +103,25 @@ def voter_registration_status(
     if value:
         status = "Success"
         summary = "Transaction request is in process"
+        response = RequestAcknowledgement(
+            TransactionId = transaction_id
+        )
     else:
         status = "Failure"
         summary = "Voter registration request not found"
+        response = RequestRejection(
+            AdditionalDetails = [
+                "The transaction ID isn't associated with any pending requests."
+            ],
+            Error = Error(
+                Name = RequestError.IDENTITY_LOOKUP_FAILED
+            ),
+            TransactionId = transaction_id
+        )
     return {
-        "id":      transaction_id,
-        "status":  status,
-        "summary": summary,
+        "status":   status,
+        "summary":  summary,
+        "response": response,
     }
 
 
@@ -109,13 +151,28 @@ def voter_registration_update(
     if value:
         status = "Success"
         summary = "Pending transaction request updated(overwritten!)"
+        response = RequestSuccess(
+            Action = [
+                SuccessAction.REGISTRATION_UPDATED,
+            ],
+            TransactionId = transaction_id
+        )
     else:
         status = "Failure"
         summary = "Voter registration request not found"
+        response = RequestRejection(
+            AdditionalDetails = [
+                "The transaction ID isn't associated with any pending requests."
+            ],
+            Error = Error(
+                Name = RequestError.IDENTITY_LOOKUP_FAILED
+            ),
+            TransactionId = transaction_id
+        )
     return {
-        "id":      transaction_id,
-        "status":  status,
-        "summary": summary,
+        "status":   status,
+        "summary":  summary,
+        "response": response,
     }
 
 
@@ -135,13 +192,28 @@ def voter_registration_cancel(
     if value:
         status = "Success"
         summary = "Transaction request has been cancelled"
+        response = RequestSuccess(
+            Action = [
+                SuccessAction.REGISTRATION_CANCELLED,
+            ],
+            TransactionId = transaction_id
+        )
     else:
         status = "Failure"
         summary = "Voter registration request not found"
+        response = RequestRejection(
+            AdditionalDetails = [
+                "The transaction ID isn't associated with any pending requests."
+            ],
+            Error = Error(
+                Name = RequestError.IDENTITY_LOOKUP_FAILED
+            ),
+            TransactionId = transaction_id
+        )
     return {
-        "id":      transaction_id,
         "status":  status,
         "summary": summary,
+        "response": response,
     }
 
 
