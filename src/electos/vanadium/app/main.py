fab89918a20e95abc66acdd788ca39b5363e130d
Ion Y <ion@osetinstitute.org>
2021-11-30 18:10:29 -0800

    Generalize initialization of application state. Setting up to share resources as fields on 'app.state'.
    Coverage: 99% (from 100%)
    
    - Setup of error handlers, event handlers and middleware not covered.
    
        src/electos/vanadium/app/main.py: 89%

--- src/electos/vanadium/app/main.py
+++ src/electos/vanadium/app/main.py
@@ -4,13 +4,77 @@ from vanadium.app.route import (
     voter_registration,
 )
 
-_routers = [
+
+# --- Application wide settings
+
+# Event name => event handler
+_EVENT_HANDLERS = {
+}
+
+# Error type => error handlers
+_ERROR_HANDLERS = {
+}
+
+# (Middleware type, parameters dict)
+_MIDDLEWARE = [
+]
+
+# Resource module names => resource initializer
+_RESOURCES = {
+}
+
+# Router module names
+_ROUTERS = [
     voter_registration,
 ]
 
-def application() -> FastAPI:
-    app = FastAPI()
-    for item in _routers:
+# --- Application setup
+
+def _setup_event_handlers(app, event_handlers):
+    """Add event handlers to the application."""
+    for name, handler in event_handlers.items():
+        app.add_event_handler(name, handler(app))
+
+
+def _setup_error_handlers(app, error_handlers):
+    """Add error handlers to the application."""
+    for name, handler in error_handlers.items():
+        app.add_error_handler(name, handler(app))
+
+
+def _setup_middleware(app, middlewares):
+    """Add middleware to the application."""
+    for call, parameters in middlewares:
+        app.add_middleware(call, **parameters)
+
+
+def _setup_routers(app, routers = None):
+    """Attach routers to the application."""
+    for item in routers:
         router = item.router()
         app.include_router(router)
+
+
+def _setup_resources(app, resources = None):
+    for name, resource in resources.items():
+        setattr(app.state, name, resource.init_resource())
+
+
+def _setup():
+    """Application initialization, including components."""
+    app = FastAPI()
+    _setup_event_handlers(app, _EVENT_HANDLERS)
+    _setup_error_handlers(app, _ERROR_HANDLERS)
+    _setup_middleware(app, _MIDDLEWARE)
+    _setup_resources(app, _RESOURCES)
+    _setup_routers(app, _ROUTERS)
+    return app
+
+
+# --- Application entry point
+
+def application() -> FastAPI:
+    """Top-level application entry point invoked by the ASGI server."""
+    app = _setup()
+    print(vars(app.state))
     return app
